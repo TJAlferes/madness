da28cf62ea6f6ede1be2fa152020de5bcbb28d97
T. J. Alferes <tjalferes@gmail.com>
2018-12-06 23:14:11 -0600

    spinning

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="signup">
+    <div class="signup-form">
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="username">
+        </div>
+        <div class="input">
+          <label for="email">Mail</label>
+          <input type="email" id="email" v-model="email">
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password">
+        </div>
+        <div class="input">
+          <label for="confirm-password">Confirm Password</label>
+          <input type="password" id="confirm-password" v-model="confirmPassword">
+        </div>
+        <div class="input inline">
+          <input type="checkbox" id="terms" v-model="terms">
+          <label for="terms">Accept Terms of Use</label>
+        </div>
+        <div class="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      terms: false
+    };
+  },
+  methods: {
+    onSubmit () {
+      const formData = {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        terms: this.terms
+      };
+      this.$store.dispatch('signup', formData);
+    }
+  }
+};
+</script>
